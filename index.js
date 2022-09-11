b972556f0b6be11aba4f49062746e354f84f60d9
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-09-11 18:45:11 +0300

    JWT Integration (#3)
    * Add: .env to ignored list
    * Add: .env.example with JWT_SECRET key
    * Dependency: Add dotenv
    * Add: dotenv integration
    * Add: Express router with XHR middleware
    * Dependency: Add jose
    * Add: /token url with post method
    * Add: Token creation example
    * Dependency: Add multer
    * Add: multer upload middleware only text fields to router
    * Add: express json middleware to router
    * Add: Request body validation
    * Add: Token verification
    * Refactor: Changed console text of start application

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
 const express = require('express');
+const jose = require('jose');
+const dotenv = require('dotenv');
+const multer = require('multer');
+
+const {createSecretKey} = require('crypto');
+
+dotenv.config(); // load the environment files
+
+const upload = multer();
+
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+router.use((req, res, next) => { // only allowed to xhr requests
+  if (req.xhr) {
+    return res.status(405)
+        .json({status: 405, message: 'XHR Not found'});
+  }
+  next();
+});
+
+router.use(upload.none()); // form-part middleware without image upload support
+router.use(express.json()); // JSON request middleware
+
+// secreyKey for JWT token
+const secretKey = createSecretKey(process.env.JWT_SECRET, 'utf-8');
+
+router.post('/token', async (req, res) => { // create's new JWT
+  const {body} = req;
+  if (!body.issuer || !body.audience) { // form-part validation
+    return res.status(422)
+        .json({status: 422, message: 'Validation error'});
+  }
+  const jwt = await new jose.SignJWT(JSON.parse(body.data))
+      .setProtectedHeader({alg: 'HS256'})
+      .setIssuedAt()
+      .setIssuer(body.issuer)
+      .setAudience(body.audience)
+      .setExpirationTime('2h')
+      .sign(secretKey);
+  return res.json({status: 200, message: '', token: jwt});
+});
+
+router.post('/verify', async (req, res) => { // verify's the JWT token
+  const {body} = req;
+  if (!body.issuer || !body.audience) { // form-part validation
+    return res.status(422)
+        .json({status: 422, message: 'Validation error'});
+  }
+  const token = req.header('Authorization').replace('Bearer ', ''); // get token
+  try {
+    const {payload, protectedHeader} = await jose.jwtVerify(token, secretKey,
+        { // set token issuer and audience
+          issuer: body.issuer,
+          audience: body.audience,
+        }); // explode the token
+    console.log(payload); // see token data
+    console.log(protectedHeader); // see token algorithm
+  } catch (e) {
+    console.error(e); // log the error
+    return res.status(422).json({status: 422, message: 'Token is invalid'});
+  }
+});
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`JWT example app listening on port ${port}`);
 });
