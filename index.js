175db29460175dc6caa6b016d31074a95c269743
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-09-11 17:50:09 +0300

    Add: Express router with XHR middleware

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const dotenv = require('dotenv');
 
 dotenv.config(); // load the environment files
 
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+router.use((req, res, next) => { // only allowed to xhr requests
+  if (req.xhr) {
+    return res.status(405)
+        .json({status: 405, message: 'XHR Not found'});
+  }
+  next();
+});
+
+router.get('/', (req, res) => {
+  res.json({message: 'test'});
+});
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
