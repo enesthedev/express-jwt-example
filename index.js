e371e75f0b487baac7ac97b48322ebc3ed693fe0
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-09-11 18:43:29 +0300

    Add: Request body validation
    Add: Token verification
    Refactor: Changed console text of start application

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const {createSecretKey} = require('crypto');
 
 dotenv.config(); // load the environment files
 
-const secretKey = createSecretKey(process.env.JWT_SECRET, 'utf-8');
-const upload = multer({storage: multer.memoryStorage});
+const upload = multer();
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
@@ -21,19 +20,47 @@ router.use((req, res, next) => { // only allowed to xhr requests
   next();
 });
 
-router.use(upload.none());
-router.use(express.json());
+router.use(upload.none()); // form-part middleware without image upload support
+router.use(express.json()); // JSON request middleware
 
-router.post('/token', async (req, res) => {
-  console.log(req.body);
-  const jwt = await new jose.SignJWT({})
+// secreyKey for JWT token
+const secretKey = createSecretKey(process.env.JWT_SECRET, 'utf-8');
+
+router.post('/token', async (req, res) => { // create's new JWT
+  const {body} = req;
+  if (!body.issuer || !body.audience) { // form-part validation
+    return res.status(422)
+        .json({status: 422, message: 'Validation error'});
+  }
+  const jwt = await new jose.SignJWT(JSON.parse(body.data))
       .setProtectedHeader({alg: 'HS256'})
       .setIssuedAt()
-      .setIssuer()
-      .setAudience('urn:example:audience')
+      .setIssuer(body.issuer)
+      .setAudience(body.audience)
       .setExpirationTime('2h')
       .sign(secretKey);
   return res.json({status: 200, message: '', token: jwt});
+});
+
+router.post('/verify', async (req, res) => { // verify's the JWT token
+  const {body} = req;
+  if (!body.issuer || !body.audience) { // form-part validation
+    return res.status(422)
+        .json({status: 422, message: 'Validation error'});
+  }
+  const token = req.header('Authorization').replace('Bearer ', ''); // get token
+  try {
+    const {payload, protectedHeader} = await jose.jwtVerify(token, secretKey,
+        { // set token issuer and audience
+          issuer: body.issuer,
+          audience: body.audience,
+        }); // explode the token
+    console.log(payload); // see token data
+    console.log(protectedHeader); // see token algorithm
+  } catch (e) {
+    console.error(e); // log the error
+    return res.status(422).json({status: 422, message: 'Token is invalid'});
+  }
 });
 
 const app = express();
@@ -42,5 +69,5 @@ const port = 3000;
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`JWT example app listening on port ${port}`);
 });
